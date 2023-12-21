f8306c3ba6331e89ad7fa16008062fd46faa770a
Roberto83d <143013856+Roberto83d@users.noreply.github.com>
2023-12-21 17:04:17 +0100

    Update app.py
    poprawiony blad wpisu

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-pip install Flask
-from flask import Flask
+from flask import Flask, render_template
 
-# Tworzenie instancji aplikacji Flask
 app = Flask(__name__)
 
-# Definicja trasy - obsługa adresu głównego ('/')
 @app.route('/')
-def hello():
-    return 'Witaj, Świecie!'
+def index():
+    return render_template('index.html')  
 
-# Uruchomienie serwera Flask
 if __name__ == '__main__':
     app.run(debug=True)
